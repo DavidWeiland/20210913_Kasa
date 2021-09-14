d273c388ee5d9fc490c9b743cd89ce802a92d229
David Weiland <weiland.david@icloud.com>
2021-09-14 14:57:52 +0200

    adding compoments to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import Banner from './Banner'
+import Body from'./Body'
+import Footer from './Footer'
 import '../styles/App.css'
+
 
 function App() {
   document.title = `kasa`
 
   return (
-    <div className='body'>
+    <div>
       <Banner />
+      <div className="app-display">
+        <Body />
+      </div>
+      <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
