0ad01337a63e10a288a7d2a081e97a2eb312637d
David Weiland <weiland.david@icloud.com>
2021-09-14 14:56:07 +0200

    Banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,7 @@
-import logo from '../assets/logo.png'
+import logo from '../assets/images/logo.png'
 import '../styles/Banner.css'
+
+// ligne 12, className lien-actif à manipuler via Router
 
 function Banner() {
   return (
@@ -7,7 +9,7 @@ function Banner() {
       <img src={logo} alt='Kasa' className='banner-logo' />
       <div>
         <ul className='banner-nav'>
-          <li className='banner-nav-elem'>
+          <li className='banner-nav-elem lien-actif'>
             Accueil
           </li>
           <li className='banner-nav-elem'>
